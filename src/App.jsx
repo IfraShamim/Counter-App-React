502f9279ec552a6317210705b675a07b1804185f
Ifra Shamim <ifrashamim29@gmail.com>
2025-01-30 17:34:51 +0500

    Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { useState } from 'react';
+import { FiRefreshCw } from "react-icons/fi"; 
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const handleRefresh = () => {
+    setCounter(0);
+  }
   return (
    <div className='rounded-lg text-center shadow w-[98%] md:w-[40%] mx-auto mt-[4rem] p-[0.5rem]'>
+    <FiRefreshCw size={30} className='text-yellow-400 mb-[1rem]' onClick={handleRefresh} style={{ cursor: "pointer",}} />
     <div className='bg-yellow-400 font-mono mb-[1rem] text-[3rem] rounded-lg text-center p-[5rem]'>{counter}</div>
     <button className='bg-gray-100 w-[5rem] mr-[4rem] font-bold text-[1.5rem] rounded cursor-pointer' onClick={()=>{
       setCounter(counter-1)
